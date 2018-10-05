a0b5223d3b945497f3eccb51756d2ea0928a2cc7
Kumaresan BK <EVOLVUS\kumaresanb@ELPBLR-035.evolvus.com>
2018-10-05 11:54:24 +0530

    OnException

--- src/main/java/com/learncamel/errorhandler/route/onexception/OnExceptionHandlerRoute.java
+++ src/main/java/com/learncamel/errorhandler/route/onexception/OnExceptionHandlerRoute.java
@@ -1,12 +1,9 @@
 package com.learncamel.errorhandler.route.onexception;
 
-import java.sql.SQLException;
-
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 
 import com.learncamel.errorhandler.bean.DataModifier;
-import com.learncamel.errorhandler.processor.GenerateErrorResponseProcessor;
 
 public class OnExceptionHandlerRoute  extends RouteBuilder {
 
@@ -14,10 +11,10 @@ public class OnExceptionHandlerRoute  extends RouteBuilder {
 	public void configure() throws Exception {
 		//onException(RuntimeException.class,Exception.class).maximumRedeliveries(2).redeliverDelay(5000).backOffMultiplier(2).log(LoggingLevel.INFO, "Exception in Bean caught here");
 	
-		onException(SQLException.class).log(LoggingLevel.INFO, "Exception in Bean caught here");
-		onException(RuntimeException.class).handled(true).maximumRedeliveries(2).delay(2000).process(new GenerateErrorResponseProcessor()).log(LoggingLevel.WARN, "Exception in Processor caught here");
-		onException(RuntimeException.class).handled(true).maximumRedeliveries(2).delay(2000).process(new GenerateErrorResponseProcessor()).to("kafka:errortopic").log(LoggingLevel.WARN, "Exception in Processor caught here");
-
+		//onException(SQLException.class).log(LoggingLevel.INFO, "Exception in Bean caught here");
+		//onException(RuntimeException.class).handled(true).maximumRedeliveries(2).delay(2000).process(new GenerateErrorResponseProcessor()).log(LoggingLevel.WARN, "Exception in Processor caught here");
+		//onException(RuntimeException.class).handled(true).maximumRedeliveries(2).delay(2000).process(new GenerateErrorResponseProcessor()).to("kafka:errortopic").log(LoggingLevel.WARN, "Exception in Processor caught here");
+		 onException(RuntimeException.class).continued(true).log(LoggingLevel.WARN, "Exception in Processor caught here");
 		
 		
 
--- src/test/java/com/learncamel/errorhandler/route/onexception/test/OnExceptionHandlerRouteTest.java
+++ src/test/java/com/learncamel/errorhandler/route/onexception/test/OnExceptionHandlerRouteTest.java
@@ -12,19 +12,29 @@ public class OnExceptionHandlerRouteTest extends CamelTestSupport {
 	protected RoutesBuilder createRouteBuilder() throws Exception {
 		return new OnExceptionHandlerRoute();
 	}
-	@Test(expected=RuntimeException.class)
+
+	@Test(expected = RuntimeException.class)
 	public void onExceptionTest() {
-		String expectedOutput="323*kumar*12SEP2018";
-		String input=null;
-		String output=template.requestBody("direct:exception",input,String.class);
-		assertEquals(expectedOutput,output);
+		String expectedOutput = "323*kumar*12SEP2018";
+		String input = null;
+		String output = template.requestBody("direct:exception", input, String.class);
+		assertEquals(expectedOutput, output);
 	}
-	
+
 	@Test
 	public void onExceptionHandeldTest() {
-		String request=null;
-		String expected="Exception happened in the route.";
-		String responce=template.requestBody("direct:exception",request,String.class);
-		assertEquals(expected,responce);
+		String request = null;
+		String expected = "Exception happened in the route.";
+		String responce = template.requestBody("direct:exception", request, String.class);
+		assertEquals(expected, responce);
+	}
+
+	@Test
+	public void onExceptionCheck_ignored() {
+		String request = null;
+		String expected = null;
+		final String responce = template.requestBody("direct:exception", request, String.class);
+		System.out.println("Responce is :" + responce);
+		assertEquals(expected, responce);
 	}
 }
